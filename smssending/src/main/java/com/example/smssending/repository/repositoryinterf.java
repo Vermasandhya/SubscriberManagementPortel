9cd5d5166e548372300779396835e11760f023f7
Vermasandhya <138787388+Vermasandhya@users.noreply.github.com>
2025-08-14 16:52:29 +0530

    Delete smssending directory

--- smssending/src/main/java/com/example/smssending/repository/repositoryinterf.java
+++
@@ -1,9 +0,0 @@
-package com.example.smssending.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.smssending.model.modelcls;
-
-public interface repositoryinterf extends JpaRepository<modelcls,Long> {
-
-}
